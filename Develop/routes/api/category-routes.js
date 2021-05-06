5201849f6bba4b0c99697da88a63fe8a3842633e
minamahmood83 <minamahmood83@gmail.com>
2021-05-06 16:53:09 -0700

    update files

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,101 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
-// The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// Get all categories and show their related products
+router.get("/", (req, res) => {
+    Category.findAll({
+            include: [{
+                model: Product,
+            }, ],
+        })
+        .then((dbCategoryData) => res.json(dbCategoryData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// Get specific category and show their related products
+router.get("/:id", (req, res) => {
+    Category.findOne({
+            where: {
+                id: req.params.id,
+            },
+            include: [{
+                model: Product,
+            }, ],
+        })
+        .then((dbCategoryData) => {
+            if (!dbCategoryData) {
+                res.status(404).json({
+                    message: "No category found with this id!",
+                });
+                return;
+            }
+            res.json(dbCategoryData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+// Create a new category
+router.post("/", (req, res) => {
+    Category.create({
+            category_name: req.body.category_name,
+        })
+        .then((dbCategoryData) => res.json(dbCategoryData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// Update a category name
+router.put("/:id", (req, res) => {
+    Category.update(req.body, {
+            //router -category
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbCategoryData) => {
+            if (!dbCategoryData) {
+                res.status(404).json({
+                    message: "No category found with this id!",
+                });
+                return;
+            }
+            res.json(dbCategoryData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// Delete a category
+router.delete("/:id", (req, res) => {
+    Category.destroy({
+            where: {
+                // id is created
+                id: req.params.id,
+            },
+        })
+        .then((dbCategoryData) => {
+            if (!dbCategoryData) {
+                res.status(404).json({
+                    message: "No category found with this id!",
+                });
+                return;
+            }
+            res.json(dbCategoryData);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,28 +1,17 @@
 const express = require("express");
 const routes = require("./routes");
-const morgan = require("morgan");
-const cors = require("cors");
+
 // import sequelize connection
 
 const app = express();
-require("dotenv").config({
-    path: "./config/connection.js",
-});
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("dev"));
-app.use(cors());
+
 app.use(routes);
-app.get("/", (req, res) => {
-    res.send("test route => home page");
-});
-app.use((req, res) => {
-    res.status(404).json({
-        msg: "page not founded",
-    });
-});
+
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
